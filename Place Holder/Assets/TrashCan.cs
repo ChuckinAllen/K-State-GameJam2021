b76f40f134182112b80c9b5bfd8ffb9d897b2476
ChuckinAllen <Richardposch11011@gmail.com>
2021-02-27 20:02:03 -0600

    Push

--- Place Holder/Assets/TrashCan.cs
+++ Place Holder/Assets/TrashCan.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class TrashCan : MonoBehaviour
 {
+    [SerializeField] GameObject can;
+
+    [SerializeField] float time;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(can, time);
     }
 }
